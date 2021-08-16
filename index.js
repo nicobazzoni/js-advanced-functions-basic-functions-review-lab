4b45c3a6fac052d50acfaf53b07a803a22995088
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-16 12:19:04 -0400

    commit tree 649e7895dfb7b854109fddaba62fdcc03c31fa3d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(act="roller-skate") {
+    return `This Saturday, I want to ${act}!`
+  }
+
+const mondayWork = function(act="go to the office") {
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(sig="*"){
+
+    return function inner(adj = "special"){
+        return `You are ${sig}${adj}${sig}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b){return a + b},
+    subtract: function(a,b){return a - b},
+    multiply: function(a,b){return a * b},
+    divide: function(a,b){return a / b}
+};
+
+function actionApplyer(num, arr){
+    let res = num
+    
+    for(let i = 0; i < arr.length; i++){
+        res = arr[i](res)
+    }
+    console.log(res)
+    return res;
+}
